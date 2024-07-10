680fba2b7e8777be5dd2b8fa34baaa67beb09fb3
Warckut <warckut@gmail.com>
2024-07-10 14:34:03 +0500

    feat: добавлен eslint, версия react изменена на 16.8

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-import terser from '@rollup/plugin-terser'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-
-const packageJson = require('./package.json')
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import packageJson from './package.json';
 
 export default [
   {
@@ -18,19 +17,11 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [
-      typescript(),
-      peerDepsExternal(),
-
-      resolve(),
-      commonjs(),
-
-      terser(),
-    ],
+    plugins: [typescript(), peerDepsExternal(), resolve(), commonjs(), terser()],
   },
   {
     input: 'dist/cjs/types/src/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts.default()]
+    plugins: [dts.default()],
   },
-]
+];
